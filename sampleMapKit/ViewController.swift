946956e9af8f978ea6c326ed7c5f8607d0a4d304
三浦宏予 <yoooon0908@yahoo.co.jp>
2016-02-11 15:04:02 +0800

    Initial Commit

---
+++ sampleMapKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleMapKit
+//
+//  Created by 三浦宏予 on 2016/02/11.
+//  Copyright © 2016年 Hiroyo Miura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ sampleMapKitTests/sampleMapKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  sampleMapKitTests.swift
+//  sampleMapKitTests
+//
+//  Created by 三浦宏予 on 2016/02/11.
+//  Copyright © 2016年 Hiroyo Miura. All rights reserved.
+//
+
+import XCTest
+@testable import sampleMapKit
+
+class sampleMapKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

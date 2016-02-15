f667529f24100300f2dbe3c60824287a661d16ea
三浦宏予 <yoooon0908@yahoo.co.jp>
2016-02-15 15:15:05 +0800

    start commit

--- sampleMapKit/ViewController.swift
+++ sampleMapKit/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myMapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //中心となる場所の座標オブジェクトを作成
+        //アヤラ
+        
+        //let coodinate = CLLocationCoordinate2DMake(10.317347, 123.905759)
+        //let coodinate = CLLocationCoordinate2DMake(64.963051, 19.020835)
+        //SM
+        let coodinate = CLLocationCoordinate2DMake(10.311715, 123.918332)
+        
+        
+        //縮尺を設定
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        
+        //範囲オブジェクトを作成
+        let region = MKCoordinateRegionMake(coodinate, span)
+        
+        //MapViewに設定する
+        myMapView.setRegion(region, animated: true)
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
